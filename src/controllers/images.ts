34e8ea84f3180a2b0ba7100d6522e1acf383ed84
Rodrigo-Flores <dev.rodrigoflores@gmail.com>
2024-12-28 10:54:22 -0300

    feat(api): :sparkles: Integrate Drive utility to fetch files in getImages endpoint

--- src/controllers/images.ts
+++ src/controllers/images.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express';
+import Drive from "@ms/utils/drive";
 
 const getImages = async (req: Request, res: Response) => {
+  const drive = new Drive();
+  const files = drive.getFiles();
+  console.log(files);
   res.json({ message: "Hello from images controller" });
 }
 
